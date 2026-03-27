f97db8752a02d2afbcabae44ed45f98eeb52d3bd
Razor <137675174+razorx411@users.noreply.github.com>
2026-03-27 09:56:59 +0700

    Delete AksaraLoka directory

--- AksaraLoka/Pages/js/contact.js
+++
@@ -1,8 +0,0 @@
-// AksaraLoka - contact.js
-
-function handleSubmit(e) {
-  e.preventDefault();
-  document.getElementById('successMsg').classList.remove('hidden');
-  document.getElementById('contactForm').reset();
-  setTimeout(() => document.getElementById('successMsg').classList.add('hidden'), 5000);
-}
